4a93c877079820d0f044146c6c03b80a777ad009
“zhaozenghui” <1594769997@qq.com>
2024-03-16 18:55:53 +0800

    feat: 内存图标的制作

--- src/views/deviceManagement/productManagement/index.vue
+++ src/views/deviceManagement/productManagement/index.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
-let deviceName = ref<string>("");
-let deviceCode = ref<string>("");
-let deviceState = ref<string>("");
-let myGroup = ref<string>("");
+let productName = ref<string>("");
+let cateName = ref<string>("");
+let state = ref<string>("");
 let current_page = ref<number>(1);
 let page_size = ref<number>(10);
 </script>
@@ -17,35 +16,24 @@ let page_size = ref<number>(10);
           <el-row>
             <el-col :xl="5" :lg="10">
               <div class="deviceName selectItem">
-                <span>设备名称</span>
-                <el-input v-model="deviceName" style="width: 150px" placeholder="请输入设备名称"></el-input>
+                <span>产品名称</span>
+                <el-input v-model="productName" style="width: 150px" placeholder="请输入设备名称"></el-input>
               </div>
             </el-col>
             <el-col :xl="5" :lg="10">
               <div class="deviceCode selectItem">
-                <span>设备编号</span>
-                <el-input v-model="deviceCode" style="width: 150px" placeholder="请输入设备名称"></el-input>
+                <span>分类名称</span>
+                <el-input v-model="cateName" style="width: 150px" placeholder="请输入设备名称"></el-input>
               </div>
             </el-col>
             <el-col :xl="5" :lg="10">
               <div class="deviceState selectItem">
                 <span>设备状态</span>
-                <el-select v-model="deviceState" style="width: 150px" placeholder="请选择设备状态">
+                <el-select v-model="state" style="width: 150px" placeholder="请选择设备状态">
                   <el-option label="未激活" value="1"></el-option>
                   <el-option label="禁用" value="2"></el-option>
                   <el-option label="离线" value="3"></el-option>
                   <el-option label="在线" value="4"></el-option>
-                </el-select>
-              </div>
-            </el-col>
-            <el-col :xl="5" :lg="10">
-              <div class="myGroup selectItem">
-                <span>我的分组</span>
-                <el-select v-model="myGroup" style="width: 150px" placeholder="请选择我的分组">
-                  <el-option label="分组1" value="1"></el-option>
-                  <el-option label="分组2" value="2"></el-option>
-                  <el-option label="分组3" value="3"></el-option>
-                  <el-option label="分组4" value="4"></el-option>
                 </el-select>
               </div>
             </el-col>
@@ -70,22 +58,22 @@ let page_size = ref<number>(10);
           <el-card body-style="height:230px" style="margin-bottom: 20px; border-radius: 20px" shadow="hover">
             <div class="header_title">
               <div class="left_title">
-                <el-icon> <Menu /> </el-icon>&nbsp;
-                <h3>监控演示</h3>
+                <el-icon>
+                  <Menu />
+                </el-icon>&nbsp;
+                <h3>⭐智能开关产品</h3>
+                &nbsp;
+                <el-tag type="info">系统</el-tag>
               </div>
               <div class="right_title">
-                <img src="@/assets/images/erweima.svg" alt="" style="width: 25px; height: 25px" />
-                <img src="@/assets/images/wifi.svg" alt="" />
+                <el-tag type="success" v-if="item % 2 == 0">已发布</el-tag>
+                <el-tag type="info" v-else>未发布</el-tag>
               </div>
             </div>
             <div class="main_body">
               <div class="left_body">
-                <div class="line1">
-                  <el-tag type="success">在线</el-tag>
-                  <el-tag type="info">离线</el-tag>
-                  <el-tag type="primary">KUHYT88</el-tag>
-                </div>
                 <div class="line2">
+                  <div class="Info">所属类型：<span>电工照明</span></div>
                   <div class="info">编号：15456489465456154654685</div>
                   <div class="info">产品：★视频监控产品</div>
                   <div class="info">激活时间：2024-01-1</div>
@@ -103,14 +91,8 @@ let page_size = ref<number>(10);
         </el-col>
       </el-row>
       <!-- 分页区域 -->
-      <el-pagination
-        v-model:current-page="current_page"
-        v-model:page-size="page_size"
-        :page-sizes="[10, 15, 20, 25]"
-        :background="true"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="400"
-      />
+      <el-pagination v-model:current-page="current_page" v-model:page-size="page_size" :page-sizes="[10, 15, 20, 25]"
+        :background="true" layout="total, sizes, prev, pager, next, jumper" :total="400" />
     </el-card>
   </div>
 </template>
